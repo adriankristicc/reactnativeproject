5c87f2e685348b51bf56e0fedee8f37bea7d5026
adriankristic <adriankristic00@icloud.com>
2026-02-09 20:44:44 +0100

    Opis izmjene

---
+++ listScreen.js
@@ -0,0 +1,32 @@
+import { Text, StyleSheet, View, FlatList } from "react-native";
+
+const students = [
+  { name: "Alek", surname: "Novakovic", age: 17 },
+  { name: "Anida", surname: "Veledar", age: 17 },
+  { name: "Danin", surname: "Zulfic", age: 17 },
+  { name: "Marko", surname: "Forcan", age: 17 },
+];
+
+const ListScreen = () => {
+  return (
+    <View>
+      <Text>ListScreen</Text>
+
+      <FlatList
+        data={students}
+        keyExtractor={(student) => student.name}
+        renderItem={({ item }) => {
+          return (
+            <Text>
+              {item.name} {item.surname} - {item.age}
+            </Text>
+          );
+        }}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+export default ListScreen;
